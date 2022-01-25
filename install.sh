0ec75d950e08bd395b4acf9a6ac8a7ba6bec681a
dawsonwen <dawsonwen123@gmail.com>
2022-01-25 18:01:35 +0800

    reconstruct install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,57 @@
+function is_link(){	
+	if [ ! -L $1 ]; then
+		return 0
+	fi
+	return 1
+}
+function is_file_exist(){
+	echo $1
+	if [ ! -f $1 ]; then
+		return 0
+	fi
+	return 1
+}
+function is_path_exist(){
+	if [ ! -d $1 ]; then
+		return 0
+	fi
+	return 1
+}
+function copy_and_backup(){
+	dir_name=$(dirname $2)
+	file_name=$(basename $2)
+	echo dir: $dir_name
+	echo file: $file_name
+	
+	if [ ! -p $dir ] ; then
+		echo "Creating "$dir
+		mkdir -p $dir
+	fi
 
-# copy and convert config file 
+	if [ -f $2 ] ;then 
+		if [ ! -L $2 ] ; then
+			echo "File" $2 "alread exist"
+			echo "back up with$2.bck"
+			mv $2 $2.bck
+		fi	
+	fi
+	echo Link $1 to $2
+	ln -s $1 $2
+}
 
 # 1. i3 config 
-mkdir ~/.i3
-cp ./i3config ~/.i3/config
-
+copy_and_backup "$(pwd)/i3config" "${HOME}/.i3/config"
 # 2. alacritty config
-cp ./alacritty.yml ~/.config/
-
+copy_and_backup "$(pwd)/alacritty.yml" "${HOME}/.config/alacritty.yml"
 # 3. compton config 
-cp ./compton.yml ~/.config/
+copy_and_backup "$(pwd)/compton.yml" "${HOME}/.config/compton.yml"
+# 4. Xmodmap
+copy_and_backup "$(pwd)/Xmodmap" "${HOME}/.Xmodmap"
+
 
 # 4. wallpapers
 mkdir ~/Pictures
 cp -r ./feh_wallpapers ~/Pictures/
 
-# 5. copy Xmodmap key remapping configruation
-cp ./Xmodmap ~/.Xmodmap
-
 # 6. evdev ???
-sduo cp ./evdev /usr/share/X11/xkb/keycodes/
-
-
+#sduo cp ./evdev /usr/share/X11/xkb/keycodes/
