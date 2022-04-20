2511fa808217d8fb17f81339ac3a0c7bf6c1ee02
Dawson <dawsonwen123@gmail.com>
2022-04-20 20:38:29 +0800

    rime 输入法配置

--- some-package-install-script/install_vmware.sh
+++ some-package-install-script/install_vmware.sh
@@ -2,6 +2,7 @@
 #  https://www.vmware.com/go/getworkstation-linux
 # 系统会自动重定向并且下载最新的vmware-linux，同理win
 
+# ZF3R0-FHED2-M80TY-8QYGC-NPKYF
 
 wget -c https://download3.vmware.com/software/WKST-1623-LX-New/VMware-Workstation-Full-16.2.3-19376536.x86_64.bundle -O /tmp/vmware_install.bundle
 sudo chmod u+x  /tmp/vmware_install.bundle
--- some-package-install-script/install_zotero.sh
+++ some-package-install-script/install_zotero.sh
@@ -1,3 +1,10 @@
-wget -qO- https://apt.retorque.re/file/zotero-apt/install.sh | sudo bash
-sudo apt update
-sudo apt install zotero
+cd /tmp
+wget -O zotero_install.tar.bz2 https://download.zotero.org/client/release/6.0.4/Zotero-6.0.4_linux-x86_64.tar.bz2
+
+unar zotero_install.tar.bz2
+sudo mv Zotero_linux-x86_64 /opt/zotero
+cd /opt/zotero
+./set_launcher_icon
+ln -s $(pwd)/zotero.desktop ~/.local/share/applications/zetero.desktop
+
+
