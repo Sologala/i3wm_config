578f36f8eb5af3ed49a16c445c6db7493f44cc28
WenDawson <dawsonwen123@gmail.com>
2024-08-19 22:28:33 +0800

    update neovim version from 0.9.0 to 0.10.0

--- some-package-install-script/ubuntu18.04/build_neovim.sh
+++ some-package-install-script/ubuntu18.04/build_neovim.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/neovim/neovim.git ./build/neovim
 cd ./build/neovim
-git checkout v0.9.0
+git checkout v0.10.0
 sudo apt-get install ninja-build gettext unzip curl build-essential
 rm -rf build
 make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local/my_install/"
