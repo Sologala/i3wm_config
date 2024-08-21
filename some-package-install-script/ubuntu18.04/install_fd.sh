10b315dc5c94e4707a6ea8adbece1b4e329944ac
WenDawson <dawsonwen123@gmail.com>
2024-08-21 21:19:33 +0800

    update fx install scipt

--- some-package-install-script/ubuntu18.04/install_fd.sh
+++ some-package-install-script/ubuntu18.04/install_fd.sh
@@ -2,6 +2,6 @@
 
 wget -c https://github.com/sharkdp/fd/releases/download/v10.1.0/fd-musl_10.1.0_amd64.deb  -O /tmp/fd.deb
 sudo dpkg -i /tmp/fd.deb
-
+sudo ln -s $(which fdfind) ~/.local/bin/fd
 wget -c https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_amd64.deb -O /tmp/ripgrep.deb
 sudo dpkg -i /tmp/ripgrep.deb
